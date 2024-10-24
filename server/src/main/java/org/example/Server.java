667d38caf0596eabb71b1041f095ae9f1c932782
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2024-10-24 20:45:51 -0300

    fix: allow multiple clients

--- server/src/main/java/org/example/Server.java
+++ server/src/main/java/org/example/Server.java
@@ -1,30 +1,33 @@
 package org.example;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) throws IOException {
-		int port = 5000; // Server port
+    public static void main(String[] args) throws IOException {
+        int port = 5000; // Server port
 
-		ServerSocket serverSocket = new ServerSocket(port);
-		System.out.println("Server listening on port " + port);
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server listening on port " + port);
+        Socket socket = serverSocket.accept();
+		
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-		while (true) {
-			Socket socket = serverSocket.accept();
-			System.out.println("Client connected to server");
+            while (true) {
+                String request = in.readLine();
+                System.out.println("Received from client: " + request);
+                String response = "Echo: " + request;
+                out.println(response);
+            }
 
-			try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-				String request = in.readLine();
-				System.out.println("Received from client: " + request);
-				String response = "Echo: " + request;
-				out.println(response);
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    }
 }
