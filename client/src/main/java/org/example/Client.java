dc717eefd625fbf3ddeaf7a03fc793c5621a0ba5
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2025-05-06 23:01:44 -0300

    feat: init latency metrics

--- client/src/main/java/org/example/Client.java
+++ client/src/main/java/org/example/Client.java
@@ -2,48 +2,74 @@ package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Random;
 
 public class Client {
     private static final String PROXY_HOST = "localhost";
     private static final int PROXY_PORT = 6000;
+    private static final int bound = 100;
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Random rand = new Random();
+
+    private static FileWriter writer;
 
     public static void main(String[] args) {
-        int numberOfThreads = Integer.parseInt(args[0]);
+        int numberOfClients = Integer.parseInt(args[0]);
         int numberOfRequests = Integer.parseInt(args[1]);
-        String op = args[2];
-        int takeInTime = Integer.parseInt(args[3]);
-        createThreads(numberOfThreads, numberOfRequests, op, takeInTime);
+        int thinkTime = Integer.parseInt(args[2]);
+        int percentRead = Integer.parseInt(args[3]);
+        createThreads(numberOfClients, numberOfRequests, thinkTime, percentRead);
     }
 
-    private static void createThreads(int numberOfThreads, int numberOfRequests, String op, int takeInTime) {
+    private static void createThreads(int numberOfThreads, int numberOfRequests, int thinkTime, int percentRead) {
+        try {
+            writer = new FileWriter("latency.txt", true);
+        } catch (IOException e) {
+            Thread.currentThread().interrupt();
+        }
+
         for (int i = 0; i < numberOfThreads; i++) {
-            new Thread(() -> runClientRequest(numberOfRequests, op, takeInTime)).start();
+            var measureLatency = i == rand.nextInt(numberOfThreads);
+            new Thread(() -> runClientRequest(numberOfRequests, measureLatency, thinkTime, percentRead)).start();
         }
     }
 
-    private static void runClientRequest(int numberOfRequests, String op, int takeInTime) {
+    private static void runClientRequest(int numberOfRequests, boolean measureLatency, int thinkTime, int percentRead) {
         try (Socket socket = new Socket(PROXY_HOST, PROXY_PORT);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-
             for (int j = 0; j < numberOfRequests; j++) {
-                int randomNumber = (int) (Math.random() * 100);
-                Message message = new Message(op, randomNumber, String.valueOf(randomNumber));
+
+                boolean readOperation = rand.nextInt(100) == percentRead;
+                Message message = new Message(readOperation ? "get" : "set", (int) Thread.currentThread().getId(), readOperation ? null : String.valueOf(j));
+
+                long start = 0;
+                long end = 0;
+                var measureCurrentRequestLatency = measureLatency && rand.nextInt(bound) == 0;
 
                 String jsonRequest = objectMapper.writeValueAsString(message);
                 System.out.println("Thread " + Thread.currentThread().getId() + " sending JSON to server: " + jsonRequest);
+
+                if (measureCurrentRequestLatency) {
+                    start = System.currentTimeMillis();
+                }
+
                 out.println(jsonRequest);
 
                 String jsonResponse = in.readLine();
+
+                if (measureCurrentRequestLatency) {
+                    end = System.currentTimeMillis();
+                    var log = String.format("Latency: %d, %d \n", start, end);
+                    writer.append(log);
+                    writer.flush();
+                }
+
                 System.out.println("Thread " + Thread.currentThread().getId() + " received JSON from server: " + jsonResponse);
 
-                Thread.sleep(takeInTime);
+                Thread.sleep(thinkTime);
             }
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
