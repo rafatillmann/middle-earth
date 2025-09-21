ddaf0d7e438b48287df1c974a6d9cbe96ae6a424
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2025-09-21 17:10:00 -0300

    refactor: add bound to args

--- client/src/main/java/org/example/Client.java
+++ client/src/main/java/org/example/Client.java
@@ -6,12 +6,12 @@ import java.io.*;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Client {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final Random rand = new Random();
-
-    private static volatile boolean running = true;
+    private static final AtomicBoolean running = new AtomicBoolean(true);
 
     private static FileWriter writer;
     private static String proxyHost;
@@ -21,25 +21,25 @@ public class Client {
         proxyHost = args[0];
         proxyPort = Integer.parseInt(args[1]);
         int numberOfClients = Integer.parseInt(args[2]);
-        int numberOfRequests = Integer.parseInt(args[3]);
-        int thinkTime = Integer.parseInt(args[4]);
-        int percentRead = Integer.parseInt(args[5]);
-        int testTime = Integer.parseInt(args[6]);
-        int valueSize = Integer.parseInt(args[7]);
+        int thinkTime = Integer.parseInt(args[3]);
+        int percentRead = Integer.parseInt(args[4]);
+        int testTime = Integer.parseInt(args[5]);
+        int valueSize = Integer.parseInt(args[6]);
+        int latencyBound = Integer.parseInt(args[7]);
 
-        createThreads(numberOfClients, numberOfRequests, thinkTime, percentRead, testTime, valueSize);
+        createThreads(numberOfClients, thinkTime, percentRead, testTime, valueSize, latencyBound);
     }
 
-    private static void createThreads(int numberOfClients, int numberOfRequests, int thinkTime, int percentRead, int testTime, int valueSize) {
+    private static void createThreads(int numberOfClients, int thinkTime, int percentRead, int testTime, int valueSize, int latencyBound) {
         try {
             writer = new FileWriter(String.format("%d-latency.csv", numberOfClients), true);
         } catch (IOException e) {
             Thread.currentThread().interrupt();
         }
 
-        new Thread(() -> runClientRequest(true, numberOfRequests, thinkTime, percentRead, valueSize)).start();
+        new Thread(() -> runClientRequest(true, thinkTime, percentRead, valueSize, latencyBound)).start();
         for (int i = 0; i < numberOfClients - 1; i++) {
-            new Thread(() -> runClientRequest(false, numberOfRequests, thinkTime, percentRead, valueSize)).start();
+            new Thread(() -> runClientRequest(false, thinkTime, percentRead, valueSize, latencyBound)).start();
         }
 
         new Thread(() -> {
@@ -48,14 +48,14 @@ public class Client {
             } catch (InterruptedException ignored) {
                 Thread.currentThread().interrupt();
             }
-            running = false;
+            running.set(false);
         }).start();
     }
 
-    private static void runClientRequest(boolean threadWillMeasureLatency, int numberOfRequests, int thinkTime, int percentRead, int valueSize) {
+    private static void runClientRequest(boolean threadWillMeasureLatency, int thinkTime, int percentRead, int valueSize, int latencyBound) {
         try {
             Socket socket = new Socket(proxyHost, proxyPort);
-            socket.setSoTimeout(3000);
+            socket.setSoTimeout(5000);
 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -66,22 +66,26 @@ public class Client {
             Message message;
             long start, end;
             boolean measureCurrentRequestLatency;
-            for (int j = 0; running && j < numberOfRequests; j++) {
+            while (running.get()) {
                 readOperation = rand.nextInt(100) < percentRead;
                 op = readOperation ? "get" : "set";
                 key = rand.nextInt(100000);
-                value = readOperation ? null : "S".repeat(valueSize); // Parametrizar (4, 1024, 4096)
+                value = readOperation ? null : "S".repeat(valueSize);
                 message = new Message(op, key, value);
                 jsonRequest = objectMapper.writeValueAsString(message);
 
                 if (threadWillMeasureLatency) {
-                    measureCurrentRequestLatency = rand.nextInt(100) == 0;
+                    measureCurrentRequestLatency = rand.nextInt(latencyBound) == 0;
                     if (measureCurrentRequestLatency) {
                         start = System.nanoTime();
-                        System.out.println(jsonRequest);
+                        // -----
                         out.println(jsonRequest);
-                        var response = in.readLine();
-                        System.out.println(response);
+                        try {
+                            in.readLine();
+                        } catch (SocketTimeoutException e) {
+                            System.out.println("No response from server: " + Thread.currentThread().getId());
+                        }
+                        // -----
                         end = System.nanoTime();
                         latency(op, start, end);
                     }
@@ -90,7 +94,7 @@ public class Client {
                     try {
                         in.readLine();
                     } catch (SocketTimeoutException e) {
-                        System.out.println(Thread.currentThread().getId() + "No response from server.");
+                        System.out.println("No response from server: " + Thread.currentThread().getId());
                     }
                 }
                 Thread.sleep(thinkTime);
@@ -103,8 +107,8 @@ public class Client {
 
     private static void latency(String operation, long start, long end) {
         try {
-            var log = String.format("%s,%d,%d,%d\n",
-                    operation, start, end, (end - start));
+            var log = String.format("%s,%d\n",
+                    operation, (end - start));
             writer.append(log);
             writer.flush();
         } catch (IOException e) {
